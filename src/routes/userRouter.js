35f4224317a79352ebb61c2b6270988220c44cf6
Isaass <isaiasoliver.dev@gmail.com>
2025-10-23 20:57:56 -0300

    fix: att middleware

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -6,22 +6,19 @@ import { authMiddleware } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.use(authMiddleware);
-
-// Troca de senha autenticado
 router.patch('/change-password', userController.changePassword);
 router.post('/request-password-reset', userController.requestPasswordReset);
-router.post('/reset-password', userController.resetPassword);
+router.post('/reset-password', authMiddleware, userController.resetPassword);
 
-router.post('/email', userController.getUserByEmail);
+router.post('/email', authMiddleware, userController.getUserByEmail);
 
-router.get('/pending', userController.getPendingUsers);
-router.get('/:id', userController.getUserById);
-router.get('/', userController.getAllUsers);
+router.get('/pending', authMiddleware, userController.getPendingUsers);
+router.get('/:id', authMiddleware, userController.getUserById);
+router.get('/', authMiddleware, userController.getAllUsers);
 
-router.patch('/:id/status', userController.updateUserStatus);
-router.patch('/:id/role', userController.assignRoleToUser);
+router.patch('/:id/status', authMiddleware, userController.updateUserStatus);
+router.patch('/:id/role', authMiddleware, userController.assignRoleToUser);
 
-router.delete('/:id', userController.remove);
+router.delete('/:id', authMiddleware, userController.remove);
 
 export default router;
